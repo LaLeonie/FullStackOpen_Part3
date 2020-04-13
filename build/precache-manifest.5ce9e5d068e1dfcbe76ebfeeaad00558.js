8857ca89fa2bc1b2757f1fe8d21335139bf1c154
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2020-04-13 16:27:47 +0100

    takes build out of gitignore

---
+++ build/precache-manifest.5ce9e5d068e1dfcbe76ebfeeaad00558.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1316a83c14dac6f8d1830b35e8bed4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "113d7f276b3222c35e21",
+    "url": "/static/js/2.3cae94eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3cae94eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c698310ee681212a29e3",
+    "url": "/static/js/main.a8f5cc00.chunk.js"
+  },
+  {
+    "revision": "d19032c1263fa01ea41a",
+    "url": "/static/js/runtime-main.08633c47.js"
+  }
+]);
