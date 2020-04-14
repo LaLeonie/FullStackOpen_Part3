73380aeceddf732fd0986237c661295a2ad1f22c
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2020-04-14 14:15:56 +0100

    exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://leonie_lea_january:${password}@cluster0-nosdz.mongodb.net/test?retryWrites=true&w=majority`;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    console.log("phonebook:");
+    result.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length == 5) {
+  const personName = process.argv[3];
+  const personNumber = process.argv[4];
+
+  const person = new Person({
+    name: personName,
+    number: personNumber,
+  });
+
+  person.save().then((response) => {
+    console.log(`added ${personName} number ${personNumber} to phonebook`);
+    mongoose.connection.close();
+  });
+}
